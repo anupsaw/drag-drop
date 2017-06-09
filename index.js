7326b368c2db09f57c634eee17441868df7fe8be
anupsaw <anupsaw@users.noreply.github.com>
2017-06-09 16:04:46 +0530

    new
    test

---
+++ index.js
@@ -0,0 +1,20 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+document.getElementById("error").style.display = "none";
+
+function drag(ev) {
+    ev.dataTransfer.setData("text\html", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text\html");
+    var element = document.getElementById(data);
+    if (element.classList.contains(ev.target.id)) {
+        document.getElementById("error").style.display = "none";
+    } else {
+        document.getElementById("error").style.display = "block";
+    }
+    ev.target.appendChild(document.getElementById(data));
+}
